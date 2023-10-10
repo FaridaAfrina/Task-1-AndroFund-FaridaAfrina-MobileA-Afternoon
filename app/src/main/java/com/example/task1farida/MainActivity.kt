be5eea9d119e6a22b8092aaf2c2587c31c08e753
Farida Afrina <faridaafrinaa.com>
2023-10-10 11:16:27 +0700

    Task Lanjutan

--- app/src/main/java/com/example/task1farida/MainActivity.kt
+++ app/src/main/java/com/example/task1farida/MainActivity.kt
@@ -5,11 +5,20 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var tv:TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        tv = findViewById(R.id.textView2)
+
+        val username =intent.getParcelableExtra<User>("User")?.username
+        val password =intent.getParcelableExtra<User>("User")?.password
+        tv.text = "Username: $username dan Password: $password"
 
         val btnInput: Button = findViewById(R.id.btn_input)
         btnInput.setOnClickListener(this)
@@ -18,12 +27,17 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
     override fun onClick(v: View) {
         when (v.id){
             R.id.btn_input -> {
-                val message = "Hallo Farida"
+//                val message = "Hallo Farida"
+//                val intent = Intent()
+//                intent.action = Intent.ACTION_SEND
+//                intent.putExtra(Intent.EXTRA_TEXT, message)
+//                intent.type = "text/plain"
+//                startActivity(intent)
+
                 val intent = Intent()
-                intent.action = Intent.ACTION_SEND
-                intent.putExtra(Intent.EXTRA_TEXT, message)
-                intent.type = "text/plain"
-                startActivity(intent)
+                intent.putExtra("history", "Berhasil Login")
+                setResult(RESULT_OK, intent)
+                finish()
             }
         }
     }
